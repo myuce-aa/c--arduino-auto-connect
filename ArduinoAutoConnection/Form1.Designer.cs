6a877a6de90db50d2230e01a851108e442441e81
Mehmet Yüce <mehmetyuce78@gmail.com>
2021-05-02 01:05:36 +0300

    C# Arduino Otomatik Bağlantı

---
+++ ArduinoAutoConnection/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ArduinoAutoConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comPortLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.messageTextbox = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(222, 28);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 23);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Bağlantı Kur";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Com Port: ";
+            // 
+            // comPortLabel
+            // 
+            this.comPortLabel.AutoSize = true;
+            this.comPortLabel.Location = new System.Drawing.Point(104, 33);
+            this.comPortLabel.Name = "comPortLabel";
+            this.comPortLabel.Size = new System.Drawing.Size(72, 13);
+            this.comPortLabel.TabIndex = 2;
+            this.comPortLabel.Text = "comPortLabel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Durum:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(104, 59);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(61, 13);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "statusLabel";
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.Location = new System.Drawing.Point(222, 59);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.disconnectBtn.TabIndex = 5;
+            this.disconnectBtn.Text = "Bağlantı Kes";
+            this.disconnectBtn.UseVisualStyleBackColor = true;
+            this.disconnectBtn.Click += new System.EventHandler(this.disconnectBtn_Click);
+            // 
+            // messageTextbox
+            // 
+            this.messageTextbox.Location = new System.Drawing.Point(107, 100);
+            this.messageTextbox.Name = "messageTextbox";
+            this.messageTextbox.Size = new System.Drawing.Size(190, 20);
+            this.messageTextbox.TabIndex = 6;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(222, 136);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 7;
+            this.sendBtn.Text = "Gönder";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mesaj";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 194);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.messageTextbox);
+            this.Controls.Add(this.disconnectBtn);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comPortLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label comPortLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button disconnectBtn;
+        private System.Windows.Forms.TextBox messageTextbox;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ArduinoAutoConnection/Form1.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoAutoConnection
+{
+    public partial class Form1 : Form
+    {
+        string com = null;
+        string VID = "XXXX";
+        string PID = "YYYY";
+
+        // Portları tarama fonksiyonu cihazınıza göre VID ve PID değerlerini girmelisiniz. Bu bilgilere göre aşağıdaki işlemde cihazlar taranıyor
+        List<string> ComPortNames()
+        {
+            String pattern = String.Format("^VID_{0}.PID_{1}", VID, PID);
+            Regex _rx = new Regex(pattern, RegexOptions.IgnoreCase);
+            List<string> comports = new List<string>();
+            RegistryKey rk1 = Registry.LocalMachine;
+            RegistryKey rk2 = rk1.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum");
+            foreach (String s3 in rk2.GetSubKeyNames())
+            {
+                RegistryKey rk3 = rk2.OpenSubKey(s3);
+                foreach (String s in rk3.GetSubKeyNames())
+                {
+                    if (_rx.Match(s).Success)
+                    {
+                        RegistryKey rk4 = rk3.OpenSubKey(s);
+                        foreach (String s2 in rk4.GetSubKeyNames())
+                        {
+                            RegistryKey rk5 = rk4.OpenSubKey(s2);
+                            RegistryKey rk6 = rk5.OpenSubKey("Device Parameters");
+                            comports.Add((string)rk6.GetValue("PortName"));
+                        }
+                    }
+                }
+            }
+            return comports;
+        }
+
+        
+        private void arduino_connection()
+        {
+            List<string> names = ComPortNames();
+            if (names.Count > 0)
+            {
+                foreach (String s in SerialPort.GetPortNames())
+                {
+                    if (names.Contains(s))
+                    {
+                        com = s;
+                    }
+                }
+            }
+            if (com != null)
+            {
+                try
+                {
+                    if (!serialPort.IsOpen && com != "")
+                    {
+                        /* Seri Port Ayarları */
+                        serialPort.PortName = com;
+                        serialPort.BaudRate = 9600;
+                        serialPort.Parity = Parity.None;
+                        serialPort.DataBits = 8;
+                        serialPort.StopBits = StopBits.One;
+                        serialPort.Open(); //Seri portu aç
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Arduino");
+                }
+            }
+            else
+                MessageBox.Show("Arduino bulunamadı!", "Arduino");
+        }
+
+        // Arduino bağlantısını kesme
+        private void arduino_disconnection()
+        {
+            if (serialPort.IsOpen) serialPort.Close(); //Seri port açıksa kapat
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Form açılışında arduinoya direkt bağlantı
+            arduino_connection();
+            comPortLabel.Text = com;
+            if (serialPort.IsOpen)
+                statusLabel.Text = "Arduino Bağlı";
+            else
+                statusLabel.Text = "Arduino Bağlı Değil";
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            arduino_connection();
+            if (serialPort.IsOpen)
+                statusLabel.Text = "Arduino Bağlı";
+            else
+                statusLabel.Text = "Arduino Bağlı Değil";
+        }
+
+        private void disconnectBtn_Click(object sender, EventArgs e)
+        {
+            arduino_disconnection();
+            if (serialPort.IsOpen)
+                statusLabel.Text = "Arduino Bağlı";
+            else
+                statusLabel.Text = "Arduino Bağlı Değil";
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine("&"+messageTextbox.Text);
+            }
+        }
+    }
+}
